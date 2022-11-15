1740f8190637b85142356d609acd65ee43236160
Hemant Sharma <hemantbeast@gmail.com>
2022-11-15 11:19:23 +0530

    Add base class for content page & view model
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

---
+++ MAUIStructure/Pages/BaseContentPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MAUIStructure.ViewModels;
+
+namespace MAUIStructure.Pages
+{
+	public class BaseContentPage : ContentPage
+	{
+		BaseViewModel _baseViewModel;
+		bool _isFirstTime;
+
+		public BaseContentPage()
+		{
+			_isFirstTime = true;
+		}
+
+		protected override void OnBindingContextChanged()
+		{
+			base.OnBindingContextChanged();
+			_baseViewModel = (BaseViewModel)BindingContext;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			if (_isFirstTime) {
+				_isFirstTime = false;
+				_baseViewModel?.OnFirstTimeAppearing();
+			} else {
+				_baseViewModel?.OnAppearing();
+			}
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			_baseViewModel?.OnDisappearing();
+		}
+	}
+}
+
---
+++ MAUIStructure/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MAUIStructure.ViewModels
+{
+	[ObservableObject]
+	public partial class BaseViewModel
+	{
+		public BaseViewModel()
+		{
+		}
+
+		public virtual void OnFirstTimeAppearing() { }
+
+		public virtual void OnAppearing() { }
+
+		public virtual void OnDisappearing() {
+			IsRefreshing = false;
+		}
+
+		[ObservableProperty]
+		public bool isRefreshing;
+	}
+}
+
